8acd88399273e2e50743660ec6eb4668867c792a
PICARD Alexandre <alexandre.picard@supinternet.fr>
2017-10-01 17:17:38 +0200

    allow user to add tasks

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,27 @@
-console.log("Hello World !");
+{
+	let vm = new Vue({
+		el : '#app',
+		data: {
+			taskTitle: '',
+			tasks : JSON.parse(localStorage.getItem('tasks')) || []
+		},
+
+		methods: {
+			refreshList() {
+				localStorage.setItem('tasks', JSON.stringify(this.tasks));
+			},
+			addTask() {
+
+				var lastId = -1;
+
+				if (this.tasks.length !== 0) {
+					lastId = this.tasks[this.tasks.length-1].id;
+				}
+
+				this.tasks.push({ id: lastId + 1, title: this.taskTitle, done: false })
+				this.taskTitle = "";
+				this.refreshList();
+			}
+		}
+	});
+}
